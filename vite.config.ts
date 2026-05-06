b9f9fc53dea5f71645b0117e86f46d6831921fc6
gpBlockchain <744158715@qq.com>
2026-05-07 00:04:41 +0800

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/fiber-wasm-user-e2e/",
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
